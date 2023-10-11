1cc7dbeadeb99daeae58ee36d7d3e9e0e27dc821
Banuyaswanth <131144656+Banuyaswanth@users.noreply.github.com>
2023-10-11 17:17:31 +0530

    Revert "implemented review section component"

--- src/App/Components/ReviewSectionTextArea/index.js
+++ src/App/Components/ReviewSectionTextArea/index.js
@@ -2,19 +2,22 @@ import { useState } from "react";
 import "./index.css";
 
 const ReviewSectionTextArea = (props) => {
-    let {reviewText,refProp} = props;
+    let {reviewText} = props;
 
     let [newReviewText, setNewReviewText] = useState(reviewText); 
 
     const HandleTextareaChange = (event) => {
-        setNewReviewText(event.target.value);
-        refProp.current.value = event.target.value;
+        setNewReviewText(event.target.value)
+    }
+
+    const GetTextOfTextArea = () => {
+        return newReviewText;
     }
 
     return (
         <div className="add-review-field-container">
             <span>Description</span>
-            <textarea ref={refProp} onChange={HandleTextareaChange} value={newReviewText} rows={8} className="add-review-text-area-field" placeholder="Write your review here..." name="review-section-textarea" id="review-section-textarea-id" />
+            <textarea onChange={HandleTextareaChange} value={newReviewText} rows={8} className="add-review-text-area-field" placeholder="Write your review here..." name="review-section-textarea" id="review-section-textarea-id" />
         </div>
     )
 }
